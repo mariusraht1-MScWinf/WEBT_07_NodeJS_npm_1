6307b27248180207f73942ae3eacaa6b25090886
mariusraht1 <mariusraht+1@gmail.com>
2021-09-16 13:10:08 +0200

    Init

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(`${chalk.gray("Hello world!")}`);
+console.log(`${chalk.blue("This program is using")} ${chalk.green.underline("CHALK")}${chalk.blue("!")}`);
